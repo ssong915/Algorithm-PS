d832d215b13188ce1001ba7b4e175f2b6c731ff3
ssong <yysk_915@naver.com>
2021-09-14 14:38:23 +0900

    [Add]: Level3

---
+++ BOS-PS step by step/Level3/10950.py
@@ -0,0 +1,5 @@
+num = int(input())
+
+for i in range(0,num):
+    A,B = map(int, input().split())
+    print(A+B)
---
+++ BOS-PS step by step/Level3/2439.py
@@ -0,0 +1,6 @@
+num = int(input())
+
+for i in range(1,num+1):
+    print(" "*(num-i)+"*"*i) 
+    # , 로 이으면 띄어쓰기 들어감 + 로 이으면 안들오감
+
