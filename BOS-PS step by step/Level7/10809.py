6247b61a4c29b9137e297ee371af7eef210cf904
ssong <yysk_915@naver.com>
2021-09-22 22:09:21 +0900

    Add: [10809.py]

--- BOS-PS step by step/Level7/10809.py
+++ BOS-PS step by step/Level7/10809.py
@@ -1,1 +1,11 @@
 S = input()
+check = [-1]*26
+
+for i in range(len(S)):
+    if check[ord(S[i])-97] != -1: #처음 나온 수가 아니면
+        continue
+    else: #처음 나온 수라면 입력
+        check[ord(S[i])-97] = i
+
+for i in check:
+    print(i,end=' ')
