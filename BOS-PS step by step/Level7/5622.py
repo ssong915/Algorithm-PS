8fe7aa6123430492f48f664271dd8872dc5b1b8c
ssong <yysk_915@naver.com>
2021-09-22 23:48:25 +0900

    Add: [5622.py]

---
+++ BOS-PS step by step/Level7/5622.py
@@ -0,0 +1,11 @@
+string = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+time = 0
+
+for i in string:
+    for j in dial :
+        if i in j:
+            time += dial.index(j)+3
+
+print(time)
+    
