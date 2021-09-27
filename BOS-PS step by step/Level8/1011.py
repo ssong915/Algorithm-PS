5f81d250aede65c377d9168d0fde7cead10acb6c
ssong <yysk_915@naver.com>
2021-09-28 02:16:44 +0900

    Add: [1011.py]

--- BOS-PS step by step/Level8/1011.py
+++ BOS-PS step by step/Level8/1011.py
@@ -1,4 +1,29 @@
-num = int(input())
+import math
 
-for _ in range(num):
-    x,y = map(int,input().split())
+N = int(input())
+
+count = 0                                    
+result = []
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    distance = b - a                        
+
+    num = math.floor(math.sqrt(distance))    
+    squared = num**2                
+
+    if distance == squared:
+        count = (num*2)-1
+
+    elif squared < distance <= squared + num:
+        count = (num*2)
+
+    elif (squared + num) < distance:
+        count = (num*2) + 1
+
+    elif distance < 4:
+        count = distance
+    result.append(count)
+
+for x in result:
+    print(x)
