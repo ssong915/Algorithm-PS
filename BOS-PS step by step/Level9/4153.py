2e11c2910a5a3d6ee30802a8a517a7441b106d15
ssong <yysk_915@naver.com>
2021-09-28 14:26:03 +0900

    Add: [4153.py]

---
+++ BOS-PS step by step/Level9/4153.py
@@ -0,0 +1,12 @@
+while True:
+    arr = list(map(int,input().split()))
+    arr.sort()
+
+    if arr[0]==arr[1]==arr[2]==0:
+        break
+
+    if arr[0]**2 + arr[1]**2 == arr[2]**2:
+        print('right')
+    else:
+        print('wrong')
+
