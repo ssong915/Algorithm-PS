ea7c5bc9e6dfcc5735ed371320e0c8a1944e3a30
ssong <yysk_915@naver.com>
2021-10-12 15:34:53 +0900

    [Add] 11_5

--- python-for-coding-test/Ch11_Greedy/5_볼링공 고르기.py
+++ python-for-coding-test/Ch11_Greedy/5_볼링공 고르기.py
@@ -2,17 +2,9 @@ n,m =  map(int, input().split())
 arr = list(map(int, input().split()))
 result = 0
 
-arr.sort()
+length = len(arr)
 
-for i in range(n):
-    j = i+1
-    while j != (n-1) :
-        if arr[i] != arr[j]:
-            result += ( n-j+1 )
-            i += 1
-            j += 1
-            break
-        else:
-            j += 1
+for num in arr:
+    result += (length - arr.count(num))
 
-print(result)
+print(int(result/2))
