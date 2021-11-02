70f532a93b1791b5edbc29b80f6bd4143d11ddb4
ssong <yysk_915@naver.com>
2021-11-02 14:38:32 +0900

    [Fix] 12_4

--- python-for-coding-test/Ch12_Implementation/4_자물쇠와 열쇠.py
+++ python-for-coding-test/Ch12_Implementation/4_자물쇠와 열쇠.py
@@ -1,9 +1,8 @@
 def rotate(a):
-    n = len(a) # 행 길이 계산
-    m = len(a[0]) # 열 길이 계산
-    result = [[0] * n for _ in range(m)] # 결과 리스트
+    n = len(a)
+    result = [[0] * n for _ in range(n)] # 결과 리스트
     for i in range(n):
-        for j in range(m):
+        for j in range(n):
             result[j][n - i - 1] = a[i][j]
     return result
 
