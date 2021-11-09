763d7d8b91196eca012d36c121676f04161d901f
ssong <yysk_915@naver.com>
2021-11-10 00:02:32 +0900

    [Fix] 12_7

--- python-for-coding-test/Ch12_Implementation/7_치킨배달.py
+++ python-for-coding-test/Ch12_Implementation/7_치킨배달.py
@@ -8,20 +8,20 @@ chicken = []
 for i in range (n):
     for j in range (n):
         if city[i][j] == 1:
-            home.append((i,j))
+            home.append((i,j)) # home 에 집좌표 추가
         elif city[i][j] == 2:
-            chicken.append((i,j))
+            chicken.append((i,j)) # chicken 에 치킨집좌표 추가
 
-pick_chicken = list(combinations(chicken,m))
+pick_chicken = list(combinations(chicken,m)) # 치킨집을 m개 씩 조합하여 list화
 
-result=[0]*len(pick_chicken)
+result=[0]*len(pick_chicken) # 조합 수 만큼의 도시치킨거리 넣어줄 배열
 
 for j in range(len(pick_chicken)):
     for i in home:
         min_dis = 100
-        for k in pick_chicken[j]:
-            temp=abs(i[0]-k[0])+abs(i[1]-k[1])
-            min_dis=min(min_dis,temp)
-        result[j]+=min_dis
+        for k in pick_chicken[j]: # j 번째 조합 loop
+            temp=abs(i[0]-k[0])+abs(i[1]-k[1]) # 치킨거리
+            min_dis=min(min_dis,temp) # j 번째 조합 중 최소 치킨거리를 찾기위해
+        result[j]+=min_dis # j 번째 도시치킨거리 += 치킨거리
 
-print(min(result))
+print(min(result)) 
