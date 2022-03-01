5563dd170646d7d7509834044babd8b8265a0259
ssong <yysk_915@naver.com>
2022-03-01 16:58:54 +0900

    [Add] 16_34

--- python-for-coding-test/Ch16_DP/34_병사배치하기.py
+++ python-for-coding-test/Ch16_DP/34_병사배치하기.py
@@ -1,11 +1,10 @@
-arr = [10, 20, 10, 30, 20, 50]
-n = len(arr)
+n = int(input())
+arr = list(map(int, input().split()))
 dp = [1] * n
- 
+
 for i in range(1, n):
-  for j in range(i):
-    if arr[j] < arr[i]:
-      dp[i] = max(dp[i], dp[j]+ 1)
- 
-print(dp)
-print(max(dp))
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
