2e15699b614b27516d3ec474c90c3f616ab9706f
ssong <yysk_915@naver.com>
2022-03-15 00:06:12 +0900

    [Add] 11054-2

--- BOS-PS step by step/Level15_DP/11054.py
+++ BOS-PS step by step/Level15_DP/11054.py
@@ -1,18 +1,20 @@
 n = int(input())
 a = list(map(int, input().split()))
-dpp = [0 for i in range(n)]
-dpm = [0 for i in range(n)]
-dpb = [0 for i in range(n)]
+
+inc_dp = [1]*n
 for i in range(n):
     for j in range(i):
-        if a[i] > a[j] and dpp[i] < dpp[j]:
-            dpp[i] = dpp[j]
-    dpp[i] += 1
+        if a[i] > a[j]:
+            inc_dp[i] = max(inc_dp[j]+1, inc_dp[i])
+
+dec_dp = [1]*n
 for i in range(n - 1, -1, -1):
     for j in range(n - 1, i, -1):
-        if a[i] > a[j] and dpm[i] < dpm[j]:
-            dpm[i] = dpm[j]
-    dpm[i] += 1
+        if a[i] > a[j]:
+            dec_dp[i] = max(dec_dp[j]+1, dec_dp[i])
+
+dp = [0]*n
 for i in range(n):
-    dpb[i] = dpp[i] + dpm[i] - 1
-print(max(dpb))
+    dp[i] = inc_dp[i]+dec_dp[i]-1
+
+print(max(dp))
