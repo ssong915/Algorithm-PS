c540817fe155875877353043ce87b06f363586ea
songkyung <yysk_915@naver.com>
2022-05-19 01:35:10 +0900

    [Add] 1406

--- BOJ/1406.py
+++ BOJ/1406.py
@@ -1,16 +1,18 @@
-stack1 = list(input())
+from sys import stdin
+
+stack1 = list(stdin.readline().strip())
 stack2 = []
 
 for _ in range(int(input())):
     command = list(input().split())
     if command[0] == 'P':
         stack1.append(command[1])
-  
+
     elif command[0] == 'L':
         if stack1:
             stack2.append(stack1.pop())
 
-    elif command[0] =='D':
+    elif command[0] == 'D':
         if stack2:
             stack1.append(stack2.pop())
 
