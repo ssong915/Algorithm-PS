069708fdbf4f4283560d7dc3abc7f40aadb5d929
songkyung <yysk_915@naver.com>
2022-07-03 01:35:10 +0900

    [Add] 1874:

--- BOJ/1874.py
+++ BOJ/1874.py
@@ -1,5 +1,25 @@
 n = int(input())
-nums = []
-for _ in range(n):
-    nums.append(int(input()))
-    
+count = 0
+stack = []
+result = []
+no_message=True
+
+for i in range(0,n):
+    x = int(input())
+
+    while count < x:
+      count += 1
+      stack.append(count)
+      result.append("+")
+
+    if stack[-1]==x:
+        stack.pop()
+        result.append("-")
+    else:
+        no_message = False
+        exit(0) 
+
+if no_message==False:
+    print("NO")
+else:
+    print("\n".join(result))
