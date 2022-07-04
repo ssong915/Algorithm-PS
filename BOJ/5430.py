bccdcb4c770258609f73084817c65583e0f7c8af
songkyung <yysk_915@naver.com>
2022-07-04 11:14:57 +0900

    [Fix] 5430

--- BOJ/5430.py
+++ BOJ/5430.py
@@ -1,34 +1,36 @@
 from collections import deque
-from sys import stdin, stdout
 
-t = int(stdin.readline())
+t = int(input())
 for _  in range(t):
-    funcs = stdin.readline()
-    length = stdin.readline().rstrip()
-    arr = stdin.readline().rstrip()[1:-1].split(',')
+    funcs = input()
+    length = int(input())
+    arr = input()[1:-1].split(',')
     queue = deque(arr)
 
     flag = 0
     err = 0 
 
-    for func in funcs:
-        if len(queue) == 0 or length == 0:
-            print('error\n')
-            err = 1
-            break
+    if length == 0:
+        queue = []
 
+    for func in funcs:
         if func == 'R':
             flag += 1
         elif func == 'D':
-            if flag % 2==0:
-                queue.popleft()
+            if len(queue) == 0:
+                print('error')
+                err = 1
+                break
             else:
-                queue.pop()
+                if flag % 2==0:
+                    queue.popleft()
+                else:
+                    queue.pop()
     
     if err == 0:
         if flag % 2 == 0:
-            print('['+','.join(queue)+']\n')
+            print('['+','.join(queue)+']')
         else:
             queue.reverse()
-            print('['+','.join(queue)+']\n')
+            print('['+','.join(queue)+']')
 
